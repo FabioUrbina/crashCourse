391d85cccb5f07604cb1900c597d977b085cbb23
Fabs <fabio@collaborationspharma.com>
2021-01-24 13:59:13 -0500

    some lessons

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+# This is a scratch code area to play with Crash Course and some simple hints.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Lists: create with var_name = []
+var = ["one","two","fewg"]
+var_name = range(1,15)
 
+# Slice using [:] syntax. var_name[:3] goes from first to 3rd, var_name[3:] goes third to last
+# create a list in one line; ** is exponential
+squares = [x**2 for x in range(1,19)]
+print(squares[-7:])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# important! Setting x=y is NOT creating a new list, but adding two names to the same list
+squareNot= squares
+squareNot.append(5)
+print("This one is exactly the same data: ", squareNot)
 
+# to create a duplicate list, must assign the whole slice from beginning to end:
+squareNot =squares[:]
+squareNot.append(5)
+print("This one is a copy: ", squareNot)
+
+# Tuples are immutable, but otherwise the same as list, just () instead of []
+dimensions = (250,50)
+print(dimensions[0])
+# dimensions[0] = 200 # throws error
+# We can overwrite a tuple, however:
+dimensions = (500,30)
+print(dimensions)
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
